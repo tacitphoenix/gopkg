2423052785a2f2b0834491739795de3d233d358d
tacitphoenix <sondiek@gmail.com>
2019-09-12 18:04:24 -0400

    added a utils package

---
+++ utils/utils.go
@@ -0,0 +1,15 @@
+package utils
+
+func Average(myInt10 10[int]) int {
+  var total, foundNum int
+  for _, value := range myInt10 {
+    if value != 0 {
+      total += value
+      foundNum += 1
+    }
+  }
+
+  return total / foundNum
+}
+
+
