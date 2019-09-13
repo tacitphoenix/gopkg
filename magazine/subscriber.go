119c114632f8974b3d1aa8312c67177e944ecb97
tacitphoenix <sondiek@gmail.com>
2019-09-13 11:47:45 -0400

    addded more structs to the magazine package

--- magazine/subscriber.go
+++ magazine/subscriber.go
@@ -1,7 +1,22 @@
+// Package magazine information about subscribers and employees
 package magazine
 
 type Subscriber struct {
-  Name string
-  Rate float64
-  Active bool
+	Name    string
+	Rate    float64
+	Active  bool
+	Address Address
+}
+
+type Employee struct {
+	Name    string
+	Salary  float64
+	Address Address
+}
+
+type Address struct {
+	Street  string
+	City    string
+	State   string
+	ZipCode int
 }
