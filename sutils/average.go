3400de5d9a828a6976352f9180a1b6fb1beac298
tacitphoenix <sondiek@gmail.com>
2019-09-13 00:43:42 -0400

    add function to calculate average from command line values

--- sutils/average.go
+++ sutils/average.go
@@ -1,6 +1,8 @@
 // Package sutils uses slices for all its computations
 package sutils
 
+import "strconv"
+
 // Average function uses slices to calcuate the average
 func Average(nums []int) int {
 	var total int
@@ -9,4 +11,17 @@ func Average(nums []int) int {
 	}
 
 	return total / len(nums)
+}
+
+// InputAvg get average of all the numbers entered from the command line
+func InputAvg(nums []string) (float64, error) {
+	var total float64
+	for _, num := range nums {
+		number, err := strconv.ParseFloat(num, 64)
+		if err != nil {
+			return total, err
+		}
+		total += number
+	}
+	return total / float64(len(nums)), nil
 }
