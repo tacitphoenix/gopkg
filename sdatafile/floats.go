23814202e3499ff0b055db1bfd328f6d2de78953
tacitphoenix <sondiek@gmail.com>
2019-09-13 00:22:50 -0400

    make it explicit that nothing was returned for the slice

--- sdatafile/floats.go
+++ sdatafile/floats.go
@@ -20,18 +20,18 @@ func GetFloats(fileName string) ([]float64, error) {
 	for scanner.Scan() {
 		number, err := strconv.ParseFloat(scanner.Text(), 64)
 		if err != nil {
-			return numbers, err
+			return nil, err
 		}
 		numbers = append(numbers, number)
 	}
 
 	err = file.Close()
 	if err != nil {
-		return numbers, err
+		return nil, err
 	}
 
 	if scanner.Err() != nil {
-		return numbers, scanner.Err()
+		return nil, scanner.Err()
 	}
 
 	return numbers, nil
