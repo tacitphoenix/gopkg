75a5b4b3b56160bd8f0a1ef28aa8b746839e7a57
tacitphoenix <sondiek@gmail.com>
2019-09-13 15:24:48 -0400

    added some setters

--- geo/coordinates.go
+++ geo/coordinates.go
@@ -1,6 +1,14 @@
 package geo
 
 type Coordinates struct {
-  Latitude float64
-  Longitude float64
+	Latitude  float64
+	Longitude float64
+}
+
+func (c *Coordinates) SetLatitude(val float64) {
+	c.Latitude = val
+}
+
+func (c *Coordinates) SetLongitude(val float64) {
+	c.Longitude = val
 }
