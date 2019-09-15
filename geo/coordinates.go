db8f1ce0897e5a30d2fc1e02d6c5beef12e5d0ff
tacitphoenix <sondiek@gmail.com>
2019-09-15 14:47:01 -0400

    add validation for coordinates

--- geo/coordinates.go
+++ geo/coordinates.go
@@ -1,20 +1,30 @@
 package geo
 
+import "errors"
+
 type Coordinates struct {
 	latitude  float64
 	longitude float64
 }
 
-func (c *Coordinates) SetLatitude(val float64) {
+func (c *Coordinates) SetLatitude(val float64) error {
+	if val < -90 || val > 90 {
+		return errors.New("invalid latitude")
+	}
 	c.latitude = val
+	return nil
 }
 
 func (c *Coordinates) Latitude() float64 {
 	return c.latitude
 }
 
-func (c *Coordinates) SetLongitude(val float64) {
+func (c *Coordinates) SetLongitude(val float64) error {
+	if val < -180 || val > 180 {
+		return errors.New("invalid longitude")
+	}
 	c.longitude = val
+	return nil
 }
 
 func (c *Coordinates) Longitude() float64 {
