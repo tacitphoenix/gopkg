ea8aa6be3be4995c1a4ef46414428cabfc603cd5
tacitphoenix <sondiek@gmail.com>
2019-09-15 18:15:44 -0400

    add gadget pakage

---
+++ gadget/tape_player.go
@@ -0,0 +1,16 @@
+package gadget
+
+import "fmt"
+
+type TapePlayer struct {
+  Batteries string
+}
+
+func (tp *TapePlayer) Play(song string) {
+  fmt.Println("Playing", song)
+}
+
+func (tp *TapePlayer) Stop(){
+  fmt.Println("Stopped")
+}
+
---
+++ gadget/tape_recorder.go
@@ -0,0 +1,20 @@
+package gadget
+
+import "fmt"
+
+type TapeRecorder struct {
+  Microphones int
+}
+
+func (tr *TapeRecorder) Record() {
+  fmt.Println("Recording")
+}
+
+func (tr *TapeRecorder) Play(song string) {
+  fmt.Println("Playing", song)
+}
+
+func (tr *TapeRecorder) Stop(){
+  fmt.Println("Stopped")
+}
+
