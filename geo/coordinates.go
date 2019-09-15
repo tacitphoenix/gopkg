ff5288e61ddce4be44a658b3d70742901f74039c
tacitphoenix <sondiek@gmail.com>
2019-09-15 14:40:07 -0400

    encapsulate coordinate

--- geo/coordinates.go
+++ geo/coordinates.go
@@ -1,14 +1,22 @@
 package geo
 
 type Coordinates struct {
-	Latitude  float64
-	Longitude float64
+	latitude  float64
+	longitude float64
 }
 
 func (c *Coordinates) SetLatitude(val float64) {
-	c.Latitude = val
+	c.latitude = val
+}
+
+func (c *Coordinates) Latitude() float64 {
+	return c.latitude
 }
 
 func (c *Coordinates) SetLongitude(val float64) {
-	c.Longitude = val
+	c.longitude = val
+}
+
+func (c *Coordinates) Longitude() float64 {
+	return c.longitude
 }
