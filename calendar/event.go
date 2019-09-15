2bd5fdd332b432343cb07b3dba6d56abfc13aa08
tacitphoenix <sondiek@gmail.com>
2019-09-15 15:12:39 -0400

    add validation method for Event title field

--- calendar/event.go
+++ calendar/event.go
@@ -1,12 +1,21 @@
 package calendar
 
+import (
+	"errors"
+	"unicode/utf8"
+)
+
 type Event struct {
 	title string
 	Date
 }
 
-func (e *Event) SetTitle(t string) {
+func (e *Event) SetTitle(t string) error {
+	if utf8.RuneCountInString(t) > 30 {
+		return errors.New("invalid title because it's too long")
+	}
 	e.title = t
+	return nil
 }
 
 func (e *Event) Title() string {
