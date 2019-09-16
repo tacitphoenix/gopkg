b8066b613123b8dbbab6b022f3dfebfee48a397d
tacitphoenix <sondiek@gmail.com>
2019-09-16 09:09:52 -0400

    wrote some tests

---
+++ prose/join_test.go
@@ -0,0 +1,11 @@
+package prose
+
+import "testing"
+
+func TestTwoElements(t *testing.T) {
+  t.Error("no test written yet")
+}
+
+func TestThreeElements(t *testing.T) {
+  t.Error("no test written here either")
+}
