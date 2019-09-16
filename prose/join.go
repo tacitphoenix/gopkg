20233dcfc7cbd3c0690e2a603a00716d949faa85
tacitphoenix <sondiek@gmail.com>
2019-09-16 10:49:29 -0400

    add some tests

--- prose/join.go
+++ prose/join.go
@@ -3,8 +3,16 @@ package prose
 import "strings"
 
 func JoinWithCommas(phrases []string) string {
-  result := strings.Join(phrases[:len(phrases)-1], ", ")
-  result += ", and "
-  result += phrases[len(phrases)-1]
-  return result
+	if len(phrases) == 0 {
+		return ""
+	} else if len(phrases) == 1 {
+		return phrases[0]
+	} else if len(phrases) == 2 {
+		return strings.Join(phrases, " and ")
+	} else {
+		result := strings.Join(phrases[:len(phrases)-1], ", ")
+		result += ", and "
+		result += phrases[len(phrases)-1]
+		return result
+	}
 }
--- prose/join_test.go
+++ prose/join_test.go
@@ -1,11 +1,31 @@
 package prose
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func TestTwoElements(t *testing.T) {
-  t.Error("no test written yet")
+type testData struct {
+	list     []string
+	expected string
 }
 
-func TestThreeElements(t *testing.T) {
-  t.Error("no test written here either")
+func TestJoinWithCommas(t *testing.T) {
+	tests := []testData{
+		testData{list: []string{}, expected: ""},
+		testData{list: []string{"apple"}, expected: "apple"},
+		testData{list: []string{"apple", "orange"}, expected: "apple and orange"},
+		testData{list: []string{"apple", "orange", "pear"}, expected: "apple, orange, and pear"},
+	}
+
+	for _, test := range tests {
+		actual := JoinWithCommas(test.list)
+		if actual != test.expected {
+			t.Errorf(errString(test.list, actual, test.expected))
+		}
+	}
+}
+
+func errString(list []string, actual string, expected string) string {
+	return fmt.Sprintf("Actual \"%s\" Expected \"%s\"", actual, expected)
 }
